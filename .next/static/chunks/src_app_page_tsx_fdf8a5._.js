cb6fd963c88b45000f4aa3f3fa5b64a8c1bbbbaa
mhgapps <jrodriguez@markethospitalitygroup.com>
2025-02-19 20:45:05 -0500

    0.0.6

--- .next/static/chunks/src_app_page_tsx_fdf8a5._.js
+++ .next/static/chunks/src_app_page_tsx_fdf8a5._.js
@@ -5,8 +5,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/src_app_page_tsx_fdf8a5._.js",
   "chunks": [
-    "static/chunks/node_modules_next_dist_ea69ac._.js",
-    "static/chunks/src_app_page_tsx_8b213b._.js"
+    "static/chunks/node_modules_9e2e38._.js",
+    "static/chunks/src_app_6a83a3._.js"
   ],
   "source": "dynamic"
 });
